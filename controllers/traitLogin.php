c6e35aaa803bf5d98c903c643f08275b17ab8a9d
Tsiry23 <tsiryrakotonirina23@gmail.com>
2024-02-12 20:20:17 +0300

    integ2

--- controllers/traitLogin.php
+++ controllers/traitLogin.php
@@ -6,11 +6,13 @@
 
     $user=login($mail,$pwd);
 
+    var_dump($user);
+
     echo count($user);
 
     if (count($user)!=0)
     {
-        header("Location:accueil.php");
+        header("Location:../pages/accueil.html");
     }
     else {
         header("Location:../pages/index.php?error=Login ou mot de passe");
