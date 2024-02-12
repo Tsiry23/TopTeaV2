b6a01213f73af1bff42c6a129cab8db7f5aee5d4
Tsiry23 <tsiryrakotonirina23@gmail.com>
2024-02-12 15:29:31 +0300

    changes in script.sql / creating documentedScript

--- documentedScript.sql
+++ documentedScript.sql
@@ -5,52 +5,52 @@ create table user (
     id int primary key  auto_increment,
     email varchar (30) not null ,
     pswd VARCHAR (255) not null ,
-    status int check (status=0 or status=1)
+    status int check (status=0 or status=1) --0 admin et 1 simple-user
 );
 
 create table teaCategory ( 
     id int primary key  auto_increment,
     name varchar(30) not null,
-    output double,
+    output double, --rendement
     CHECK (output>=0)
 );
 
 create table parcel (
     id int primary key  auto_increment,
-    size double ,
+    size double , --m²
     idTeaCategory int REFERENCES teaCat,
     startDate date not null
 );
 
-create table picker (
+create table picker ( --cueilleur
     id int primary key  auto_increment,
     name varchar (40)
 );
 
 
-create table picking (
+create table picking ( --cueillette
     id int primary key  auto_increment,
     idParcel int REFERENCES parcel(id),
     qty double DEFAULT 0 ,
     theDate date not null ,
-    CHECK (qty>=0)
+    CHECK (output>=0)
 );
 
 create table salary (
-    salary double DEFAULT 5000,
+    salary double DEFAULT 5000, -- salaire pour 1 kg
     salaryDate date not null
 );
 
-create table categSpent (
+create table categSpent ( --catégorie dépense
     id int primary key  auto_increment,
     name VARCHAR (30)
 );
 
-create table spent (
+create table spent ( --dépenses
     id int primary key  auto_increment,
     idcategSpent int,
     spent double,
     theDate date,
     CHECK (spent>=0),
-    Foreign key (idcategSpent) REFERENCES categSpent(id)
+    Foreign key idcategSpent REFERENCES categSpent(id)
 );
