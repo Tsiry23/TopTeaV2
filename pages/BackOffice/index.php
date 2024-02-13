a0c274e71c9db949238c1ee54814165402c84582
fah40 <159771399+fah40@users.noreply.github.com>
2024-02-13 13:48:31 +0100

    Update index.php
    ajustement index

--- pages/BackOffice/index.php
+++ pages/BackOffice/index.php
@@ -23,12 +23,15 @@
         <div class="log-container">
             <div class="log">
                 <div class="log-panel log-right" style="color: hsl(118, 33%, 43%)">
-                    <img src="assets/logo2.png" alt="miandry" width="100" height="100">
+                    <img src="../../assets/logo2.png" alt="miandry" width="100" height="100">
                     <h1>TopTea</h1>
                     <p>Hello, Friend!</p>
                 </div>
             </div>
         </div>
+    </div>
+    <div style="text-align: center;bottom: 5px;position: fixed;">
+    <p>ETU002719 - ETU002575 - ETU002363</p>
     </div>
     <script>
         <?php if (isset($_GET["error"])) { ?>
